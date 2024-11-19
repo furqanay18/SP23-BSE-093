337372bce00ccccaeeb3f6346033126531b60fb6
furqanay18 <mfurqanali18@gmail.com>
2024-11-19 18:18:14 +0500

    Assignment Nodewithmyfirstlayout

---
+++ NODE/index.js
@@ -0,0 +1,19 @@
+const express =require ('express');
+let server=express();
+
+server.set("view engine","ejs");
+server.use(express.static("public"))
+server.get("/",(req,res)=>{
+    res.render("Homepage");
+});
+
+server.get("/portfolio", (req,res) =>{
+    res.render("portfolio")
+});
+
+server.listen(5020, () => {
+    console.log("server Started at localhost:5020");
+})
+
+
+
